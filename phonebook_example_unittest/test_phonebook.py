20b5676329795141e32e8ef1f6889b7083fe6343
Charlie O'Hara <charlie.b.ohara@gmail.com>
2017-09-12 14:38:05 -0700

    update notes

--- phonebook_example_unittest/test_phonebook.py
+++ phonebook_example_unittest/test_phonebook.py
@@ -2,12 +2,20 @@ import unittest
 from phonebook import Phonebook
 
 class PhonebookTest(unittest.TestCase):
-    def test_create_phonebook(self):
-        phonebook = Phonebook()
+    def setUp(self):
+        self.phonebook = Phonebook()
 
     def test_lookup_entry_by_name(self):
-        phonebook = Phonebook()
-        phonebook.add("Bob", "12345")
+        self.phonebook.add("Bob", "12345")
         # first argument is the expected value
         # second argument is the actual value from the system under test
-        self.assertEqual("12345", phonebook.lookup("Bob"))
+        self.assertEqual("12345", self.phonebook.lookup("Bob"))
+
+    def test_missing_entry_raises_KeyError(self):
+        with self.assertRaises(KeyError):
+            self.phonebook.lookup("missing")
+
+    @unittest.skip("WIP")
+    def test_empty_phonebook_is_consistent(self):
+        phonebook = Phonebook()
+        self.assertTrue(self.phonebook.is_consistent())
